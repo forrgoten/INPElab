8a33a512b8a2b09882ae19849ee224c920709ea8
forrgoten <dolinoffdima@gmail.com>
2024-10-20 20:44:32 +0300

    added paperwork

--- lab3inf/main.cpp
+++ lab3inf/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     float x,y;
     cin >> x >> y;
-    if((y < 1 - x*x && y>x && x>=0) || (x<=0 && y>=0 && y-x<=1))
+    if((y < 1 - x*x && y>0 && y<1) || (x < 0 && x > -1 && y>0 && y - x < 1))
         {
             cout << "YES";
         }
--- lab4inf/mainaltalt.cpp
+++ lab4inf/mainaltalt.cpp
@@ -5,16 +5,27 @@ using namespace std;
 
 int main()
 {
-    double  x, xn, xk, dx, a;
+    int curr=1;
+    double  x, xn, xk, dx, a, zmin, zmax;
+    zmax=0;
     a = 2.94;
     xn = 1.5;
     xk = 5.5;
     dx = 0.4;
+    zmin = a*pow((a*pow(xn,7))/(cos(a*a*a) + 1.31),1/5.0);
     x = xn;
     do
         {
+        if(curr % 2 == 0)
             cout << a*pow((a*pow(x,7))/(cos(a*a*a) + 1.31),1/5.0) << endl;
-            x =x + dx;
+
+        if(zmax < a*pow((a*pow(x,7))/(cos(a*a*a) + 1.31),1/5.0))
+            zmax = a*pow((a*pow(x,7))/(cos(a*a*a) + 1.31),1/5.0);
+        if(zmin > a*pow((a*pow(x,7))/(cos(a*a*a) + 1.31),1/5.0))
+            zmin = a*pow((a*pow(x,7))/(cos(a*a*a) + 1.31),1/5.0);
+            curr++;
+            x+=dx;
         }while(x <= xk);
+    cout << zmax - zmin;
     return 0;
 }
